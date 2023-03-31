901ea5460167c7b91f7ec32c3d0679d60690372c
Psort <skowronpiotr.00@gmail.com>
2023-03-31 14:32:56 +0200

    Add Route

--- src/router/App.router.tsx
+++ src/router/App.router.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { Navbar } from "../components/navbar/Navbar";
 import {Home} from "../components/home/Home";
+import {menuItems} from "./menuItems";
 
 export const AppRouter = () => {
   return (
     <Routes>
-      <Route path="/" element={<Navbar />}>
+      <Route path="/" element={<Navbar  menuItems={menuItems}/>}>
         <Route index element={<Home />} />
       </Route>
     </Routes>
