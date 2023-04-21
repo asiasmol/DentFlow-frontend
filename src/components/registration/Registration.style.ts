a9b176dd8fa7aba7c6253843cfd416f983c265fb
PrzemekKomorowski <106514184+PrzemekKomorowski@users.noreply.github.com>
2023-04-21 08:54:57 +0200

    css login/registration refactor

--- src/components/registration/Registration.style.ts
+++ src/components/registration/Registration.style.ts
@@ -4,6 +4,8 @@ import TextField from "@mui/material/TextField";
 
 
 export const WindowRegistration = styled.div`
+  position: relative;
+  top: -40px;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -14,7 +16,7 @@ export const StyledBox = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  background-color: #c5d4ff;
+  background-color: #1784B3;
   padding: 2rem;
   width: 25rem;
   border-radius: 3rem;
@@ -25,7 +27,7 @@ export const StyledBox = styled.div`
   }
 `
 export const Fields = styled.div`
-  margin: 4rem;
+  margin: 3rem;
   @media(max-width: 768px){
     margin: 1rem;
   }
@@ -46,5 +48,42 @@ export const InputField = styled.input`
   width: 246px;
   height: 50px;
   border-radius: 10px;
+`
+
+export const RegisterButton = styled.button`
+    margin-top: 40px;
+  margin-left:40px;
+  bottom: 10%;
+  width: 320px;
+  padding: 16px;
+  border: 0;
+  background-color: rgb(255, 190, 92);
+  border-radius: 8px;
+  color: white;
+  font-weight: 600;
+  font-size: 16px;
+  cursor: pointer;
+
+  &:hover {
+    filter: brightness(85%);
+  }
+  
+  
+  @media (max-width: 767px) {
+    bottom:-40%;
+    width:15rem;
+    margin-left: auto;
+    margin-right: auto;
+  }
+  @media (max-width: 450px) {
+    bottom:-40%;
+    font-size: 0.8rem;
+    width:5rem;
+    height: 3rem;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 5px;
+    padding:0;
+  }
 `
 
