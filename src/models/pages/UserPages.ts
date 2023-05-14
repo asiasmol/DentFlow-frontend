04515413f4b7a861f6444e13bfbabee3b8b2cdb2
Psort <skowronpiotr.00@gmail.com>
2023-05-14 14:55:32 +0200

    refactor userPage

---
+++ src/models/pages/UserPages.ts
@@ -0,0 +1,12 @@
+import {link} from "../Link";
+
+export const UserPages: link[] = [
+    {
+        label: "Moje Wizyty",
+        link: "/Visits"
+    },
+    {
+        label: "Umów wizytę",
+        link: "/clinics"
+    },
+];
